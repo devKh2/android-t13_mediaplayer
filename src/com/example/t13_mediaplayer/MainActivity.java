e76ac0b0d9a995d657a161f396e45f05f47add4e
b <b@a-PC>
2014-09-25 19:37:43 +0900

    Media Player

---
+++ src/com/example/t13_mediaplayer/MainActivity.java
@@ -0,0 +1,77 @@
+package com.example.t13_mediaplayer;
+
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+
+public class MainActivity extends ActionBarActivity {
+
+	
+	@Override
+	protected void onDestroy(){
+		if( mp != null ){
+			mp.stop();
+			mp.release();
+		}
+		mp = null;
+		
+		super.onDestroy();
+	}
+	
+	MediaPlayer mp = null;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+        Button btnStart = (Button)findViewById(R.id.btnStart);
+        Button btnStop = (Button)findViewById(R.id.btnEnd);
+        btnStart.setOnClickListener(new OnClickListener() {
+			
+        	@Override
+			public void onClick(View v) {
+			// TODO Auto-generated method stub
+				mp = MediaPlayer.create(MainActivity.this, R.raw.kalimba);
+				mp.start();
+			}
+		});
+        btnStop.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				if(mp != null){
+					mp.stop();
+					mp.release();
+				}
+				mp = null;
+			}
+		});
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
